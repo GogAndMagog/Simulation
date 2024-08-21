db6654aaf26f6fb9ccf69e866446eb550b440605
GogAndMagog <sergejsmel6@gmail.com>
2024-08-21 16:40:01 +0300

    feat(DistanceCalculator): make Distance Calculators singleton

--- src/main/java/org/simulation/service/Graphs/DistanceCalculationHeuristic/ChebyshevDistanceCalculator.java
+++ src/main/java/org/simulation/service/Graphs/DistanceCalculationHeuristic/ChebyshevDistanceCalculator.java
@@ -3,6 +3,18 @@ package org.simulation.service.Graphs.DistanceCalculationHeuristic;
 import org.simulation.service.Graphs.Entities.Coordinates;
 
 public class ChebyshevDistanceCalculator implements DistanceCalculator {
+
+    private static DistanceCalculator instance = new ChebyshevDistanceCalculator();
+
+    private ChebyshevDistanceCalculator() {}
+
+    public static DistanceCalculator getInstance() {
+        if (instance == null) {
+            instance = new ChebyshevDistanceCalculator();
+        }
+        return instance;
+    }
+
     @Override
     public int calculateDistance(Coordinates from, Coordinates to) {
         return Math.max(Math.abs(from.getX() - to.getX()), Math.abs(from.getY() - to.getY()));
