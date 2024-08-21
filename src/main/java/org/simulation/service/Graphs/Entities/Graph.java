ac2bb982a3ed74d1cd5e88db13e60df02aead2a0
GogAndMagog <sergejsmel6@gmail.com>
2024-08-21 16:51:59 +0300

    feat(Graph): added common logic for getting node by coordinates

--- src/main/java/org/simulation/service/Graphs/Entities/Graph.java
+++ src/main/java/org/simulation/service/Graphs/Entities/Graph.java
@@ -3,4 +3,5 @@ package org.simulation.service.Graphs.Entities;
 public interface Graph<Id, Node> {
     public void addNode(Node node);
     public Node getNodeById(Id node);
+    public Node getNodeByCoordinates(Coordinates coordinates);
 }
