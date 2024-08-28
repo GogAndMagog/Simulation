64b86bde358bd88526a63114bf78951c930bab18
GogAndMagog <sergejsmel6@gmail.com>
2024-08-28 15:43:26 +0300

    fix(Wolf): fixed movement-method

--- src/main/java/org/simulation/model/entities/dynamic/carnivore/Wolf.java
+++ src/main/java/org/simulation/model/entities/dynamic/carnivore/Wolf.java
@@ -7,26 +7,15 @@ import org.simulation.model.entities.dynamic.herbivore.Herbivore;
 import org.simulation.model.entities.statical.terrain.Terrain;
 import org.simulation.service.Graphs.AStarAlgorithmQueue;
 import org.simulation.service.Graphs.DistanceCalculationHeuristic.ChebyshevDistanceCalculator;
-import org.simulation.service.Graphs.DistanceCalculationHeuristic.DistanceCalculator;
 import org.simulation.service.Graphs.Entities.Coordinates;
+import org.simulation.service.Graphs.Entities.Graph;
+import org.simulation.service.Graphs.Entities.Node;
 import org.simulation.service.Graphs.GraphFabric.AStarGraphFactory;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Wolf extends Carnivore {
-    private int visionRange = 10;
-
-
-    public static void main(String[] args) {
-        Wolf wolf = new Wolf(new Coordinates(2, 2), 5, 1, 2);
-
-        WorldMap wp = new WorldMap(3, 3);
-
-        wp.setCreature(wolf);
-        wp.setCreature(new Wolf(new Coordinates(0, 0), 5, 1, 2));
-        wp.setCreature(new Wolf(new Coordinates(1, 0), 5, 1, 2));
-    }
 
     public Wolf(Coordinates position, int actionPoints, int hp, int attack) {
         super(position, actionPoints, hp, attack);
@@ -66,14 +55,22 @@ public class Wolf extends Carnivore {
     }
 
     private int movement(Integer actionPointsPerTurn, Creature target) {
-        var graph = graphAbstractFabric.createGraph(this.worldMap);
+        Graph<String, Node> graph = graphAbstractFabric.createGraph(this.worldMap);
         List<Coordinates> path = pathFinder.findPath(graph,
-                graph.getNodeByCoordinates(this.getPosition()),
-                graph.getNodeByCoordinates(target.getPosition()));
+                graph.getNodeByCoordinates(this.getPosition()).getId(),
+                graph.getNodeByCoordinates(target.getPosition()).getId());
+
+        if (path == null) {
+            return actionPointsPerTurn;
+        }
+
+        //remove current position and position of target creature
+        path.removeLast();
+        path.removeFirst();
 
         Iterator<Coordinates> pathIterator = path.iterator();
         Coordinates currentCoordinates;
-        while (pathIterator.hasNext() || actionPointsPerTurn > 0) {
+        while (pathIterator.hasNext() && actionPointsPerTurn > 0) {
             currentCoordinates = pathIterator.next();
             var landscapeObject = this.worldMap.getLandscape().get(currentCoordinates);
             if (landscapeObject instanceof Terrain) {
