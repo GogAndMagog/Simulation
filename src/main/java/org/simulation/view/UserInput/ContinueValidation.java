8a0cfc51d146dd9b18e9af0bf69bbedb12128730
GogAndMagog <sergejsmel6@gmail.com>
2024-09-03 20:06:52 +0300

    style: delete unused classes

--- src/main/java/org/simulation/view/UserInput/ContinueValidation.java
+++
@@ -1,8 +0,0 @@
-package org.simulation.view.UserInput;
-
-public class ContinueValidation implements UserInputValidation {
-    @Override
-    public boolean isValid(String input) {
-        return input.equals("c") || input.equals("C") || input.equals("Continue");
-    }
-}
