48accbf2abd0d4c8e2b72643d151b83ce7c2dfee
GogAndMagog <sergejsmel6@gmail.com>
2024-09-03 20:01:02 +0300

    refactor(WorldMap): delete unused methods and commented code

--- src/main/java/org/simulation/model/entities/WorldMap.java
+++ src/main/java/org/simulation/model/entities/WorldMap.java
@@ -12,8 +12,6 @@ public class WorldMap {
 
     private HashMap<Coordinates, LandscapeObject> landscape = new HashMap<>();
     private HashMap<Coordinates, Creature> creatures = new HashMap<>();
-//    List<Creature> creatures = new ArrayList<>();
-//    List<LandscapeObject> landscape = new ArrayList<>();
 
     public WorldMap(int x, int y) {
         this.y = y;
@@ -31,7 +29,6 @@ public class WorldMap {
     public void setLandscapeObject(LandscapeObject object) {
         if (checkOutOfField(object.getPosition())) {
             landscape.put(object.getPosition(), object);
-//            landscape.add(object);
             object.setWorldMap(this);
         }
     }
@@ -39,7 +36,6 @@ public class WorldMap {
     public void setCreature(Creature creature) {
         if (checkOutOfField(creature.getPosition())) {
             creatures.put(creature.getPosition(), creature);
-//            creatures.add(creature);
             creature.setWorldMap(this);
         }
     }
@@ -58,32 +54,6 @@ public class WorldMap {
 
     public void removeLandscapeObjet(Coordinates position) {
         landscape.remove(position);
-    }
-
-    private double calculateLength(Coordinates current, Coordinates target) {
-        double length = 0.0;
-        length = Math.sqrt(Math.pow((current.getX() - target.getX()), 2)
-                + Math.pow((current.getY() - target.getY()), 2));
-
-        return length;
-    }
-
-    private Map<Coordinates, Double> getDistancesToTarget(Coordinates currentPosition, Class target) {
-        Map<Coordinates, Double> creaturesDistances = new HashMap<>();
-
-//        creatures.values().stream()
-//                .filter(target::isInstance)
-//                .filter(creature -> creature.getPosition() != currentPosition)
-//                .forEach(creature -> creaturesDistances.put(creature.getPosition(), calculateLength(currentPosition, creature.getPosition())));
-
-        return creaturesDistances;
-    }
-
-    public Optional<Coordinates> getClosest(Coordinates currentPosition, Class target) {
-        return getDistancesToTarget(currentPosition, target)
-                .entrySet()
-                .stream().min(Comparator.comparingDouble(Map.Entry::getValue))
-                .map(creature -> creature.getKey());
     }
 
     public List<Coordinates> getNeighbours(Coordinates coordinates, Coordinates target) {
@@ -125,8 +95,7 @@ public class WorldMap {
     }
 
     private boolean isAccessible(Coordinates coordinates, Coordinates target) {
-        return checkOutOfField(coordinates) &&
-                checkObstacles(coordinates, target);
+        return checkOutOfField(coordinates);
     }
 
     private boolean checkOutOfField(Coordinates coordinates) {
@@ -138,28 +107,4 @@ public class WorldMap {
         else
             return true;
     }
-
-    private boolean checkObstacles(Coordinates coordinates, Coordinates target) {
-
-//        if (coordinates != null && coordinates.equals(target))
-//            return true;
-//
-//        if (landscape.get(coordinates) != null && !(landscape.get(coordinates) instanceof Terrain))
-//            return false;
-//
-//        if (creatures.get(coordinates) != null)
-//            return false;
-
-        return true;
-    }
-
-//    public int getPassability(Coordinates coordinates) {
-//        LandscapeObject landscapeObject = landscape.get(coordinates);
-//
-//        if (landscapeObject != null && landscapeObject instanceof Terrain)
-//            return ((Terrain) landscapeObject).getPassability() + 1;
-//        else
-//            return 1;
-//    }
-
 }
