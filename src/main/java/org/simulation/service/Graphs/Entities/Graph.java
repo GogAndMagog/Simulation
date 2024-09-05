96ed5510447e6b7ae74aef27c801257e8f1b0e6f
GogAndMagog <sergejsmel6@gmail.com>
2024-09-05 14:15:19 +0300

    style(Interfaces): remove unnecessary "public" keyword

--- src/main/java/org/simulation/service/Graphs/Entities/Graph.java
+++ src/main/java/org/simulation/service/Graphs/Entities/Graph.java
@@ -1,7 +1,7 @@
 package org.simulation.service.Graphs.Entities;
 
 public interface Graph<Id, Node> {
-    public void addNode(Node node);
-    public Node getNodeById(Id node);
-    public Node getNodeByCoordinates(Coordinates coordinates);
+    void addNode(Node node);
+    Node getNodeById(Id node);
+    Node getNodeByCoordinates(Coordinates coordinates);
 }
