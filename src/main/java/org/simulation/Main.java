a18251c78f951ff7b47482df6b3d0ae55c93abd4
GogAndMagog <sergejsmel6@gmail.com>
2024-09-06 21:34:16 +0300

    style(Main): remove commented and unused

--- src/main/java/org/simulation/Main.java
+++ src/main/java/org/simulation/Main.java
@@ -3,10 +3,8 @@ package org.simulation;
 import org.simulation.view.userinput.usercontroller.UserControllerConsoleController;
 import org.simulation.view.userinput.usercontroller.UserControllerContext;
 
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         UserControllerContext userControllerContext = new UserControllerConsoleController();
         userControllerContext.showScreen();
     }
